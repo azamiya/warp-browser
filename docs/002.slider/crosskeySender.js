e405600be4f4f4580fcd5a641d223c88e231c669
cdra0624 <tatsukiadaniya@tatsuki.local>
2016-11-15 08:45:26 +0900

    parseInt slider

--- docs/002.slider/crosskeySender.js
+++ docs/002.slider/crosskeySender.js
@@ -31,6 +31,5 @@ document.addEventListener("keyup", function (e) {
 
 function outputUpdate(vol) {
   document.querySelector('#volume').value = vol;
-  vol = vol * 1000000;
   if(conn != null) conn.send(vol);
 }
