81b8dbea31caa2c7b170f57119f68204328de92f
hrl7 <hrlclb@gmail.com>
2016-11-15 16:58:05 +0900

    add peer list

--- docs/002.slider/peerCommunicate.js
+++ docs/002.slider/peerCommunicate.js
@@ -1,6 +1,11 @@
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-var peer = new Adawarp();
+var user = location.hash.replace(/#?user=(\w+)/, "$1");
+var peer = new Adawarp({
+  credential: {
+    username: user ? user : null
+  }
+});
 var conn;
 var call;
 
@@ -16,7 +21,36 @@ window.onload = function(){
     peer.on('open', function(){
         document.getElementById("myID").innerHTML = peer.id;
     });
+    peer.on("updatePeerList", function(peers) {
+        console.log(peers);
+        setPeerList(peers);
+    });
     peer.login();
+}
+
+function setPeerList(peerInfoList) {
+  let list = document.getElementById("peer-list");
+    
+  list.innerHTML = "";
+  for (let i in peerInfoList) {
+    (function(info) { 
+      if (peer.info.name === info.name) {
+        return ;
+      }
+     let e = document.createElement("li");
+     let text = document.createElement("span");
+     e.textContent = `${i}: ${peerInfoList[i].name}`;
+     let ringButton = document.createElement("button");
+     ringButton.textContent = "Connect";
+     ringButton.onclick = function() {
+       document.getElementById("partner-id-input").value = info.id;
+       callStart();
+     };
+     e.append(text);
+     e.append(ringButton);
+     list.appendChild(e);
+    })(peerInfoList[i]);
+  }
 }
 
 function displayMyCamera(){
